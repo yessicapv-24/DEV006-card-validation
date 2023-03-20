5b9ec13908367213f7cef92546cb0f72bbf4194a
Yessica Peñaloza Vilchis <yess@MacBook-Air-de-Yessica.local>
2023-03-19 22:41:52 -0600

    reestructuracion

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import validator from "./validator";
+
 function validar() {
   var numTarjeta = document.getElementById("numTarjeta").value;
   if (numTarjeta.length ===0) {
@@ -24,28 +26,9 @@ function validar() {
     return;
   }
 
+  var numTarjetaValid = validator.isValid(numTarjeta);
 
-  var array = numTarjeta.split("");
-  var arrayReversed = array.reverse();
-  var suma = 0;
-
-  for (var i = 0; i < arrayReversed.length; i++) {
-    if (i % 2 == 1) {
-      var mult = parseInt(arrayReversed[i]) * 2;
-      if (mult > 9) {
-        var multString = mult.toString();
-        var digitos = parseInt(multString.charAt(0)) + parseInt(multString.charAt(1));
-        suma = suma + digitos;
-      } else {
-        suma = suma + mult;
-      }
-    } else {
-      suma = suma + parseInt(arrayReversed[i]);
-    }
-  }
-  console.log(suma);
-
-  if (suma % 10 === 0) {
+  if (numTarjetaValid === true) {
     alert("Transacción exitosa");
   } else {
     alert("Tarjeta inválida");
@@ -55,12 +38,6 @@ function validar() {
 function rellenar(element) {
   if (element.value.length >  element.maxLength) 
     element.value = element.value.slice(0, element.maxLength);
-  
-  if(element.value.length == element.maxLength){
-    var lastDigits = element.value.slice(12, element.maxLength);
-    var hiddenNumber = "#### #### #### " + lastDigits;
-    var numero = document.getElementById("numero");
-    numero.innerHTML = hiddenNumber;
-  }
-}
 
+  numero.innerHTML = validator.maskify(element.value);
+}
